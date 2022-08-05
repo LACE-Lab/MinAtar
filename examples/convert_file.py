1cd0fb296daf7dd126d151a0b006ff999d9efce5
Zoe Shao <zoshao@g.hmc.edu>
2022-08-05 10:16:48 -0700

    Convert result file format

---
+++ examples/convert_file.py
@@ -0,0 +1,16 @@
+games = ["space_invaders", "asterix", "breakout", "seaquest", "freeway"]
+for game in games:
+    PATH = f"/research/erin/zoshao/results/2022_08_03_{game}.txt"
+    result_path = f"2022_08_03_{game}.results"
+    with open(PATH, 'r') as f:
+        f.write("Score"+"\t"+"#Frames"+"\n")
+        for l in f:
+            sp = l.split()
+            print(sp)
+            r = sp[8]
+            frame = sp[11]
+            print(r, frame)
+            with open(result_path, "a") as d:
+                d.write(str(r)+"\t"+str(frame)+"\n")
+            d.close()
+    f.close()
