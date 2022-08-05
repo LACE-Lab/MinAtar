564c4eaa6fb404bcda1a79ee3f026f4494758a55
Zoe Shao <zoshao@g.hmc.edu>
2022-08-05 10:22:38 -0700

    For 8/1 file

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -1,16 +1,18 @@
 games = ["space_invaders", "asterix", "breakout", "seaquest", "freeway"]
+dims = ["128", "256"]
 for game in games:
-    PATH = f"/research/erin/zoshao/results/2022_08_03_{game}.txt"
-    result_path = f"/research/erin/zoshao/results/2022_08_03_{game}.results"
-    with open(PATH, 'r') as f:
-        with open(result_path, "a") as d:
-            d.write("Score"+"\t"+"#Frames"+"\n")
-        d.close()
-        for l in f:
-            sp = l.split()
-            r = sp[8]
-            frame = sp[11]
+    for dim in dims:
+        PATH = f"/research/erin/zoshao/results/2022_08_01_{game}_{dim}.txt"
+        result_path = f"/research/erin/zoshao/results/2022_08_01_{game}_{dim}.results"
+        with open(PATH, 'r') as f:
             with open(result_path, "a") as d:
-                d.write(str(r)+"\t"+str(frame)+"\n")
+                d.write("Score"+"\t"+"#Frames"+"\n")
             d.close()
-    f.close()
+            for l in f:
+                sp = l.split()
+                r = sp[8]
+                frame = sp[11]
+                with open(result_path, "a") as d:
+                    d.write(str(r)+"\t"+str(frame)+"\n")
+                d.close()
+        f.close()`
