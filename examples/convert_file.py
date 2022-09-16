072248c8925c75c4e35a58f07886cc664d00e140
Zoe Shao <zoshao@g.hmc.edu>
2022-09-16 11:21:05 -0700

    Fixed path to convert files

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -2,8 +2,8 @@ games = ["space_invaders", "asterix", "breakout", "seaquest", "freeway"]
 suffixes = ["AC_encoded", "AC", "-k", "-k2"]
 for game in games:
     for suffix in suffixes:
-        PATH = f"/research/erin/zoshao/results/2022_09_09_{game}.txt"
-        result_path = f"/research/erin/zoshao/results/2022_09_09_{game}.results"
+        PATH = f"/research/erin/zoshao/results/2022_09_09_{game}_{suffix}.txt"
+        result_path = f"/research/erin/zoshao/results/2022_09_09_{game}_{suffix}.results"
         with open(PATH, 'r') as f:
             for l in f:
                 sp = l.split()
