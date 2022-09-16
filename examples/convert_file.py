78eb8fc9db8af140a99f6cba36a6d386f789796e
Zoe Shao <zoshao@g.hmc.edu>
2022-09-16 11:08:57 -0700

    Convert result files

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -1,7 +1,7 @@
 games = ["space_invaders", "asterix", "breakout", "seaquest", "freeway"]
-suffix = ["AC_encoded", "AC", "-k", "-k2"]
+suffixes = ["AC_encoded", "AC", "-k", "-k2"]
 for game in games:
-    for dim in dims:
+    for suffix in suffixes:
         PATH = f"/research/erin/zoshao/results/2022_09_09_{game}.txt"
         result_path = f"/research/erin/zoshao/results/2022_09_09_{game}.results"
         with open(PATH, 'r') as f:
