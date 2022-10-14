e8064e3f8480858cfb92448236b551e556ce3863
Zoe Shao <zoshao@g.hmc.edu>
2022-10-14 11:22:41 -0700

    Convert files

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -6,8 +6,8 @@ for game in games:
     for suffix1 in suffixes1:
         for suffix2 in suffixes2:
             for suffix3 in suffixes3:
-                PATH = f"/research/erin/zoshao/results/2022_10_07_{game}_{suffix1}_{suffix2}{suffix3}.txt"
-                result_path = f"/research/erin/zoshao/results/2022_10_07_{game}_{suffix1}_{suffix2}{suffix3}.results"
+                PATH = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.txt"
+                result_path = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.results"
                 with open(PATH, 'r') as f:
                     for l in f:
                         sp = l.split()
