73b081b8f261e854d6b232b7dd968ae74b11c51b
Zoe Shao <zoshao@g.hmc.edu>
2022-10-14 11:23:23 -0700

    Convert files

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -1,19 +1,17 @@
 games = ["breakout"]
 suffixes1 = ["ac"]
 suffixes2 = ["0.01","0.001","0.0001", "0.00001", "0.000001"]
-suffixes3 = ["_decoder"]
 for game in games:
     for suffix1 in suffixes1:
         for suffix2 in suffixes2:
-            for suffix3 in suffixes3:
-                PATH = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.txt"
-                result_path = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.results"
-                with open(PATH, 'r') as f:
-                    for l in f:
-                        sp = l.split()
-                        r = sp[8]
-                        frame = sp[11]
-                        with open(result_path, "a") as d:
-                            d.write(str(r)+"\t"+str(frame)+"\n")
-                        d.close()
-                f.close()
+            PATH = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.txt"
+            result_path = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.results"
+            with open(PATH, 'r') as f:
+                for l in f:
+                    sp = l.split()
+                    r = sp[8]
+                    frame = sp[11]
+                    with open(result_path, "a") as d:
+                        d.write(str(r)+"\t"+str(frame)+"\n")
+                    d.close()
+            f.close()
