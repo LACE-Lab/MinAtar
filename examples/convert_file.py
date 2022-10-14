325d4a774e7ccafe32a13bcfb6ba063b1f890285
Zoe Shao <zoshao@g.hmc.edu>
2022-10-14 11:23:43 -0700

    Convert files

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -4,8 +4,8 @@ suffixes2 = ["0.01","0.001","0.0001", "0.00001", "0.000001"]
 for game in games:
     for suffix1 in suffixes1:
         for suffix2 in suffixes2:
-            PATH = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.txt"
-            result_path = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}{suffix3}.results"
+            PATH = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}.txt"
+            result_path = f"/research/erin/zoshao/results/2022_10_12_{game}_{suffix1}_{suffix2}.results"
             with open(PATH, 'r') as f:
                 for l in f:
                     sp = l.split()
