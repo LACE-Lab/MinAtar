30787a6f81e778131f11d3ea2b83c6ef0db62af1
Zoe Shao <zoshao@g.hmc.edu>
2022-12-02 11:29:09 -0800

    Convert

--- examples/convert_file.py
+++ examples/convert_file.py
@@ -1,13 +1,13 @@
 import pstats
 
-games = ["breakout"]
-suffixes1 = ["ac","dqn"]
-suffixes2 = ["0.000001", "0.00001", "0.0001", "0.001"]
+games = ["breakout","test","test2"]
+suffixes1 = ["qr_dqn"]
+suffixes2 = ["0.00001", "0.00005", "0.0001", "16_0.00001","16_00005","16_0001"]
 for game in games:
     for suffix1 in suffixes1:
         for suffix2 in suffixes2:
-            PATH = f"/research/erin/zoshao/results/2022_10_18_{game}_{suffix1}_{suffix2}.txt"
-            result_path = f"/research/erin/zoshao/results/2022_10_18_{game}_{suffix1}_{suffix2}.results"
+            PATH = f"/research/erin/zoshao/results/2022_11_28_{game}_{suffix1}_{suffix2}.txt"
+            result_path = f"/research/erin/zoshao/results/2022_11_28_{game}_{suffix1}_{suffix2}.results"
             with open(PATH, 'r') as f:
                 for l in f:
                     sp = l.split()
@@ -17,6 +17,7 @@ for game in games:
                         d.write(str(r)+"\t"+str(frame)+"\n")
                     d.close()
             f.close()
+            print(f"/research/erin/zoshao/results/2022_11_28_{game}_{suffix1}_{suffix2}")
     
 # file = open('/research/erin/zoshao/results/2022_10_28_profile_results_ac_0.00001_no_opt_tottime.txt', 'w')
 # profile = pstats.Stats('/research/erin/zoshao/results/2022_10_28_profile_results_ac_0.00001_no_opt', stream=file)
