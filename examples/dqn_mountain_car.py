08f9a89451f0721bc00baefa85f411349a00edb9
Zoe Shao <zoshao@g.hmc.edu>
2023-02-21 20:52:02 -0800

    Fixed a bug caused by using GPU

--- examples/dqn_mountain_car.py
+++ examples/dqn_mountain_car.py
@@ -403,7 +403,7 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
             action = choose_action(s_cont, policy_net, EPSILON, num_actions)
             print(env.step(action.item()))
             if cpu == False:
-                s_cont_prime, reward, is_terminated, _ = env.step(action.item())[0]
+                s_cont_prime, reward, is_terminated, _, _ = env.step(action.item())
                 s_cont_prime = s_cont_prime[0]
             else:
                 s_cont_prime, reward, is_terminated, _ = env.step(action.item())
