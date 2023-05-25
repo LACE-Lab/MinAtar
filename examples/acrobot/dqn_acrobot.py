8677ec8c200ac1056c0180ed3b081d72576c9410
Zoe Shao <zoshao@g.hmc.edu>
2023-05-25 13:53:23 -0700

    Transferred to Acrobot

--- examples/acrobot/dqn_acrobot.py
+++ examples/acrobot/dqn_acrobot.py
@@ -1,21 +1,5 @@
 ################################################################################################################
-# Authors:                                                                                                     #
-# Kenny Young (kjyoung@ualberta.ca)                                                                            #
-# Tian Tian(ttian@ualberta.ca)                                                                                 #
-#                                                                                                              #
-# python3 dqn.py -g <game>                                                                                     #
-#   -o, --output <directory/file name prefix>                                                                  #
-#   -v, --verbose: outputs the average returns every 1000 episodes                                             #
-#   -l, --loadfile <directory/file name of the saved model>                                                    #
-#   -a, --alpha <number>: step-size parameter                                                                  #
-#   -s, --save: save model data every 1000 episodes                                                            #
-#   -r, --replayoff: disable the replay buffer and train on each state transition                              #
-#   -t, --targetoff: disable the target network                                                                #
-#                                                                                                              #
-# References used for this implementation:                                                                     #
-#   https://pytorch.org/docs/stable/nn.html#                                                                   #
-#   https://pytorch.org/docs/stable/torch.html                                                                 #
-#   https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html                                   #
+# This file contains original DQN training with Cart Pole. It does not contain MVE.
 ################################################################################################################
 
 import torch
@@ -26,38 +10,34 @@ import torch.optim as optim
 import time
 import gym
 
-import random, numpy, argparse, logging, os
+import random, argparse, logging, os
 
 import numpy as np
-from tqdm import tqdm
 
 from collections import namedtuple
-from environment import Environment
-from velenvironment import Velenvironment
 
 ################################################################################################################
 # Constants
-#
 ################################################################################################################
 BATCH_SIZE = 32
-REPLAY_BUFFER_SIZE = 100000
-TARGET_NETWORK_UPDATE_FREQ = 500
+REPLAY_BUFFER_SIZE = 10000
+TARGET_NETWORK_UPDATE_FREQ = 256
 TRAINING_FREQ = 1
-NUM_FRAMES = 5000000
-FIRST_N_FRAMES = 1000
-REPLAY_START_SIZE = 500
+NUM_FRAMES = 10000000
+FIRST_N_FRAMES = 100
+REPLAY_START_SIZE = 64
 END_EPSILON = 0.1
-STEP_SIZE = 0.5
+STEP_SIZE = 0.003
+WEIGHT_DECAY = 0.0001
 GRAD_MOMENTUM = 0.95
 SQUARED_GRAD_MOMENTUM = 0.95
 MIN_SQUARED_GRAD = 0.01
-GAMMA = 0.99
-EPSILON = 0.1
-H = 10 # rollout constant
+GAMMA = 1.0
+EPSILON = 1
+SEED = 42
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cpu")
-
 
 ################################################################################################################
 # class QNetwork
@@ -97,24 +77,6 @@ class QNetwork(pl.LightningModule, nn.Module):
         # Returns the output from the fully-connected linear layer
         return self.output(x)
 
-# Define the approximate model of the environment
-class Model(nn.Module):
-    def __init__(self, state_dim, action_dim, hidden_dim=32):
-        super(Model, self).__init__()
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, 2 * state_dim + 1)
-    
-    def forward(self, state, action):
-        x = torch.cat([state, action], dim=1)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        next_state, reward, done = x[:, :self.state_dim], x[:, self.state_dim:-1], x[:, -1]
-        return next_state, reward, done
-
 ###########################################################################################################
 # class replay_buffer
 #
@@ -142,26 +104,6 @@ class replay_buffer:
 
     def sample(self, batch_size):
         return random.sample(self.buffer, batch_size)
-
-
-################################################################################################################
-# get_state
-#
-# Converts the state given by the environment to a tensor of size (in_channel, 10, 10), and then
-# unsqueeze to expand along the 0th dimension so the function returns a tensor of size (1, in_channel, 10, 10).
-#
-# Input:
-#   s: current state as numpy array
-#
-# Output: current state as tensor, permuted to match expected dimensions
-#
-################################################################################################################
-def get_state(s):
-    return (torch.tensor(s, device=device).permute(2, 0, 1)).unsqueeze(0).float()
-
-def get_cont_state(cont_s, max_obj=40):
-    cont_s = np.array(cont_s)
-    return torch.tensor(cont_s, device=device).unsqueeze(0).unsqueeze(0).float()
 
 
 ################################################################################################################
@@ -232,84 +174,22 @@ def train(sample, policy_net, target_net, optimizer):
     loss.backward()
     optimizer.step()
     
-    
-def choose_action(state, policy_net, epsilon, n_actions):
+def choose_action(t, replay_start_size, state, policy_net, n_actions):
     # print(state)
     x = state.to(device).clone().detach().unsqueeze(0)
-
+    epsilon = END_EPSILON if t - replay_start_size >= FIRST_N_FRAMES \
+        else ((END_EPSILON - EPSILON) / FIRST_N_FRAMES) * (t - replay_start_size) + EPSILON
+    
     # epsilon-greedy
     if np.random.uniform() < epsilon: # random
         action = torch.tensor([random.randint(0, n_actions-1)]).to(device)
+
     else: # greedy
         actions_value = policy_net(x) # score of actions
         action = torch.max(actions_value, 1)[1].data.numpy() # pick the highest one
         action = torch.tensor(action).to(device)
 
     return action
-
-# Define a function for computing the rollout
-def rollout(sample, env, policy_net, target_net, H):
-    # Batch is a list of namedtuple's, the following operation returns samples grouped by keys
-    batch_samples = transition(*zip(*sample))
-
-    # states, next_states are of tensor (BATCH_SIZE, in_channel, 10, 10) - inline with pytorch NCHW format
-    # actions, rewards, is_terminal are of tensor (BATCH_SIZE, 1)
-    sample_states = torch.cat(batch_samples.state)
-    sample_next_states = torch.cat(batch_samples.next_state)
-    sample_actions = torch.cat(batch_samples.action)
-    sample_rewards = torch.cat(batch_samples.reward)
-    sample_is_terminal = torch.cat(batch_samples.is_terminal)
-    
-    # Initialize variables for the rollout
-    state = sample_states[0]
-    # print("state: ", state)
-    states = []
-    actions = []
-    rewards = []
-    done = False
-    total_reward = 0
-    
-    # Perform the rollout for H steps or until done
-    for h in range(H):
-        states.append(state)
-        action = choose_action(state, policy_net, EPSILON, env.action_space.n)
-        actions.append(action)
-        next_state, reward, done, _ = env.step(action.item())
-        rewards.append(reward)
-        total_reward += reward
-        state = next_state
-        if done:
-            break
-    print("rollout reward: ", rewards)
-    
-    # Compute the value estimates for each state using the model
-    values = []
-    for state, action in zip(states, actions):
-        state = state.unsqueeze(0)
-        value = target_net(state).detach().max(1)[0]
-        values.append(value)
-    print("rollout values: ", values)
-        
-    # Compute the discounted rewards
-    discounted_rewards = []
-    running_reward = 0
-    for reward in rewards[::-1]:
-        running_reward = reward + 0.99 * running_reward
-        discounted_rewards.insert(0, running_reward)
-    print("rollout discounted reward: ", discounted_rewards)
-        
-    # Normalize the discounted rewards
-    discounted_rewards = torch.tensor(discounted_rewards).float().to(device)
-    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-5)
-    
-    # Compute the loss
-    loss = 0
-    for value, discounted_reward in zip(values, discounted_rewards):
-        loss += (value - discounted_reward) ** 2
-    print("loss: ", loss)
-        
-    return loss, total_reward
-
 
 ################################################################################################################
 # dqn
@@ -328,8 +208,19 @@ def rollout(sample, env, policy_net, target_net, H):
 #   step_size: step-size for RMSProp optimizer
 #
 #################################################################################################################
-def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result=False, load_path=None, step_size=STEP_SIZE):
+def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result=False, load_path=None, step_size=STEP_SIZE, seed=SEED):
+    
+    # Set up the seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
     torch.set_num_threads(1)
+    
     # Get channels and number of actions specific to each game
     in_channels = env.observation_space.shape[0]
     num_actions = env.action_space.n
@@ -345,7 +236,6 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
         r_buffer = replay_buffer(REPLAY_BUFFER_SIZE)
         replay_start_size = REPLAY_START_SIZE
 
-    criterion = nn.MSELoss()
     optimizer = optim.RMSprop(policy_net.parameters(), lr=step_size, alpha=SQUARED_GRAD_MOMENTUM, centered=True, eps=MIN_SQUARED_GRAD)
 
     # Set initial values
@@ -390,31 +280,29 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
     e = e_init
     policy_net_update_counter = policy_net_update_counter_init
     t_start = time.time()
-    for t in tqdm(range(NUM_FRAMES)):
+    while t <= NUM_FRAMES:
         # Initialize the return for every episode (we should see this eventually increase)
         G = 0.0
 
         # Initialize the environment and start state
         cpu = True
         
-        if type(env.reset()) != numpy.ndarray:
+        if type(env.reset()) != np.ndarray:
             cpu = False
             s_cont = torch.tensor(env.reset()[0], dtype=torch.float32).to(device)
         else:
             s_cont = torch.tensor(env.reset(), dtype=torch.float32).to(device)
-        # s_cont = torch.tensor(env.reset(), device=device)
+        
         is_terminated = False
-        while (not is_terminated) and t < NUM_FRAMES:
-            
-            if e % 500 == 0:
-                env.render()
+        while (not is_terminated):
             # Generate data
-            action = choose_action(s_cont, policy_net, EPSILON, num_actions)
+            action = choose_action(t, replay_start_size, s_cont, policy_net, num_actions)
+            
             if cpu == False:
                 s_cont_prime, reward, is_terminated, _, _ = env.step(action.item())
             else:
                 s_cont_prime, reward, is_terminated, _ = env.step(action.item())
-            s_cont_prime = s_cont_prime
+
             s_cont_prime = torch.tensor(s_cont_prime, dtype=torch.float32, device=device)
 
             sample = None
@@ -428,18 +316,15 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
                 if t > REPLAY_START_SIZE and len(r_buffer.buffer) >= BATCH_SIZE:
                     # Sample a batch
                     sample = r_buffer.sample(BATCH_SIZE)
+                    sample_env = r_buffer.sample(BATCH_SIZE)
 
             # Train every n number of frames defined by TRAINING_FREQ
             if t % TRAINING_FREQ == 0 and sample is not None:
                 if target_off:
                     train(sample, policy_net, policy_net, optimizer)
-                    # print("rollouttime")
-                    # rollout(sample, env, policy_net, policy_net, H)
                 else:
                     policy_net_update_counter += 1
-                    # print("rollouttime")
                     train(sample, policy_net, target_net, optimizer)
-                    # rollout(sample, env, policy_net, target_net, H)
 
             # Update the target network only after some number of policy network updates
             if not target_off and policy_net_update_counter > 0 and policy_net_update_counter % TARGET_NETWORK_UPDATE_FREQ == 0:
@@ -451,7 +336,6 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
 
             # Continue the process
             s_cont = s_cont_prime
-            # print(is_terminated, t)
 
         # Increment the episodes
         e += 1
@@ -462,13 +346,16 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
 
         # Logging exponentiated return only when verbose is turned on and only at 1000 episode intervals
         avg_return = 0.99 * avg_return + 0.01 * G
-        if e % 500 == 0:
+        if e % 5 == 0:
             logging.info("Episode " + str(e) + " | Return: " + str(G) + " | Avg return: " +
-                         str(numpy.around(avg_return, 2)) + " | Frame: " + str(t)+" | Time per frame: " +str((time.time()-t_start)/t) )
+                         str(np.around(avg_return, 2)) + " | Frame: " + str(t)+" | Time per frame: " +str((time.time()-t_start)/t) )
 
             f = open(f"{output_file_name}.txt", "a")
             f.write("Episode " + str(e) + " | Return: " + str(G) + " | Avg return: " +
                          str(np.around(avg_return, 2)) + " | Frame: " + str(t)+" | Time per frame: " +str((time.time()-t_start)/t) + "\n" )
+            f.close()
+            f = open(f"{output_file_name}.results", "a")
+            f.write(str(np.around(avg_return, 2)) + "\t" + str(t) + "\n")
             f.close()
         # Save model data and other intermediate data if the corresponding flag is true
         if store_intermediate_result and e % 1 == 0:
@@ -486,7 +373,7 @@ def dqn(env, replay_off, target_off, output_file_name, store_intermediate_result
             }, output_file_name + "_checkpoint")
 
     # Print final logging info
-    logging.info("Avg return: " + str(numpy.around(avg_return, 2)) + " | Time per frame: " + str((time.time()-t_start)/t))
+    logging.info("Avg return: " + str(np.around(avg_return, 2)) + " | Time per frame: " + str((time.time()-t_start)/t))
         
     # Write data to file
     torch.save({
@@ -504,6 +391,7 @@ def main():
     parser.add_argument("--loadfile", "-l", type=str)
     parser.add_argument("--alpha", "-a", type=float, default=STEP_SIZE)
     parser.add_argument("--save", "-s", action="store_true")
+    parser.add_argument("--seed", "-d", type=int, default=SEED)
     parser.add_argument("--replayoff", "-r", action="store_true")
     parser.add_argument("--targetoff", "-t", action="store_true")
     args = parser.parse_args()
@@ -523,12 +411,12 @@ def main():
         load_file_path = args.loadfile
 
     env = gym.make("Acrobot-v1")
+    env.seed(args.seed)
 
     print('Cuda available?: ' + str(torch.cuda.is_available()))
-    dqn(env, args.replayoff, args.targetoff, file_name, args.save, load_file_path, args.alpha)
+    dqn(env, args.replayoff, args.targetoff, file_name, args.save, load_file_path, args.alpha, args.seed)
 
 
 if __name__ == '__main__':
     main()
-
 
