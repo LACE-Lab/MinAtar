e59733ef1914e9847a36fe5d1988245e6d86aa01
Zoe Shao <zoshao@g.hmc.edu>
2023-05-31 10:24:25 -0700

    fixed the bug

--- examples/acrobot/customAcrobot.py
+++ examples/acrobot/customAcrobot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gym
 from gym.envs.classic_control import AcrobotEnv
+import torch
 
 class CustomAcrobot(AcrobotEnv):
     def __init__(self):
@@ -8,7 +9,8 @@ class CustomAcrobot(AcrobotEnv):
 
     def set_state_from_observation(self, obs):
         assert len(obs) == 6, "Observation should be of length 6: (cos(theta1), sin(theta1), cos(theta2), sin(theta2), theta1dot, theta2dot)"
-        obs = obs.detach().numpy()
+        if isinstance(obs, torch.Tensor):
+            obs = obs.detach().cpu().numpy()
         theta1 = np.arctan2(obs[1], obs[0])
         theta2 = np.arctan2(obs[3], obs[2])
         theta1dot = obs[4]
